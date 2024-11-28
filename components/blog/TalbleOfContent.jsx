cbc2b2b0c21e7d3139ccbdaddede66c6cdc2b2dc
josei <koga1.jyo.73@gmail.com>
2024-11-29 00:12:38 +0900

    目次

--- components/blog/TalbleOfContent.jsx
+++ components/blog/TalbleOfContent.jsx
@@ -1,7 +1,7 @@
 export const TableOfContents = ({ toc }) => {
   return (
     <div className="bg-gray-200 p-6 rounded-lg">
-      <p className="text-lg mb-4">Contents</p>
+      <p className="text-lg mb-4 font-bold">目次</p>
       <ul>
         {toc.map(data => (
           <li key={data.id} className={`contents_head_${data.name} mb-3`}>
